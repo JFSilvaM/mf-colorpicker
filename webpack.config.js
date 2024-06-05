d9ae665a1ab6ef73815d4a7227e7bb9b6ebfccc2
joaofsma <joaofsma@ext.inditex.com>
2024-06-05 09:34:01 +0200

    nueva URL de produccion

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require("dotenv-webpack");
 const deps = require("./package.json").dependencies;
 module.exports = (_, argv) => ({
   output: {
-    publicPath: "https://main--regal-selkie-1ff6b4.netlify.app/",
+    publicPath: "https://mfcolorpicker.netlify.app/",
   },
 
   resolve: {
